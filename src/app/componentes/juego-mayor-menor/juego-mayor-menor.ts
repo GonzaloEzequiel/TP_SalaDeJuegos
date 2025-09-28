9e1c7fb57748ecd488a8edee71f26917bd87de4e
Gonzalo Ezequiel Pérez <gonzaloezequielperez@buenosaires.gob.ar>
2025-09-28 13:38:48 -0300

    v1.2.1 3erPush

--- src/app/componentes/juego-mayor-menor/juego-mayor-menor.ts
+++ src/app/componentes/juego-mayor-menor/juego-mayor-menor.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 import { UserData } from '../../models/user-data';
-import { MazoRespuesta, RobarRespuesta } from '../../models/mayorMenor-response';
 
 @Component({
   standalone: false,
@@ -20,34 +19,83 @@ export class JuegoMayorMenor {
   imagenCartaNueva :string = "";
   valorCartaVieja :number = 0;
   valorCartaNueva :number = 0;
-  puntje :number = 0;
+  puntaje :number = 0;
 
   constructor(private http: HttpClient, private router :Router) {}
 
   ngOnInit() {
-    this.nuevoJuego();
+    
   }
 
   nuevoJuego() {
-    this.susbcripcion = this.http.get<MazoRespuesta>('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2')
+    this.puntaje = 0;
+
+    this.susbcripcion = this.http.get<any>('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2')
     .subscribe(res => {
-      if(res.exito)
-        this.mazo = res.idMazo;
+      if (res.success)
+        this.mazo = res.deck_id;
       else
         console.error("Falló la API para obtener el mazo");
+
+      this.robarCarta();
     });
   }
 
-  robarCarta() {
-    this.http.get<RobarRespuesta>(`https://deckofcardsapi.com/api/deck/${this.mazo}/draw/?count=2`)
+  async robarCarta() {
+
+    console.log(this.valorCartaVieja);
+    console.log(this.valorCartaNueva);
+
+
+
+    this.valorCartaVieja = this.valorCartaNueva;
+    this.imagenCartaVieja = this.imagenCartaNueva;
+
+    this.http.get<any>(`https://deckofcardsapi.com/api/deck/${this.mazo}/draw/?count=1`)
     .subscribe(res => {
-      if(res.exito) {
-        this.imagenCartaNueva = res.cartas[0].imagen;
-        this.valorCartaNueva = parseInt(res.cartas[0].valor);
+      if(res.success) {
+        this.imagenCartaNueva = res.cards[0].image;
+        switch(res.cards[0].value) {
+          case 'ACE':
+            this.valorCartaNueva = 1;
+            break;
+          case 'JACK':
+            this.valorCartaNueva = 11;
+            break;
+          case 'QUEEN':
+            this.valorCartaNueva = 12;
+            break;
+          case 'KING':
+            this.valorCartaNueva = 13;
+            break;
+          default:
+            this.valorCartaNueva = parseInt(res.cards[0].value);
+            break;
+        }                  
       }
       else
         console.error("Falló la API para robar carta");
     });
+
+  }
+
+  async apostar(opcion :string) {
+
+    this.robarCarta()
+    .then(() => {
+
+      console.log(`Apuesta: ${this.valorCartaNueva} ${opcion} ${this.valorCartaVieja}`);
+
+      if( (opcion === "menor" && this.valorCartaNueva < this.valorCartaVieja) ||
+          (opcion === "mayor" && this.valorCartaNueva > this.valorCartaVieja) )
+        this.puntaje += 2;
+      else if(opcion === "igual" && this.valorCartaNueva == this.valorCartaVieja) 
+        this.puntaje += 50;
+      else
+        this.puntaje --;
+
+    });
+    
   }
 
   ngOnDestroy() {
